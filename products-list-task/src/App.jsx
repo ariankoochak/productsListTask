b063637391781dd9616912d419098af15b738e6e
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-08-25 22:28:31 +0330

    add other routes

--- products-list-task/src/App.jsx
+++ products-list-task/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from './layouts/MainPage';
 import Shop from './layouts/Shop';
+import Categories from './layouts/Categories';
+import ContactUs from './layouts/ContactUs';
+import AboutUs from './layouts/AboutUs';
 
 export default function App() {
   return (
@@ -9,6 +12,9 @@ export default function App() {
           <Routes>
               <Route path="/" element={<MainPage />}></Route>
               <Route path="/shop" element={<Shop />}></Route>
+              <Route path="/categories" element={<Categories />}></Route>
+              <Route path="/contact-us" element={<ContactUs />}></Route>
+              <Route path="/about-us" element={<AboutUs />}></Route>
           </Routes>
       </BrowserRouter>
   );
---
+++ products-list-task/src/layouts/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function AboutUs() {
+  return (
+    <>
+    <Navbar selected={"About us"}/>
+    </>
+  )
+}

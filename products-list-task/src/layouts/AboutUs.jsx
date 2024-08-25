469809fb3b8892476991f7b64dd6ba74a7da19f9
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-08-25 23:04:10 +0330

    add about us page

--- products-list-task/src/layouts/AboutUs.jsx
+++ products-list-task/src/layouts/AboutUs.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
 
 export default function AboutUs() {
-  return (
-    <>
-    <Navbar selected={"About us"}/>
-    </>
-  )
+    return (
+        <>
+            <Navbar selected={"About us"} />
+            <div className="about-us">
+                <h2>About Us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Officia ipsa amet at? Unde voluptate sequi atque, eum nulla,
+                    ad quis eligendi assumenda error autem id, ea officia
+                    temporibus quia fuga. Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Officia ipsa amet at? Unde
+                    voluptate sequi atque, eum nulla, ad quis eligendi assumenda
+                    error autem id, ea officia temporibus quia fuga. Lorem ipsum
+                    dolor sit amet, consectetur adipisicing elit. Officia ipsa
+                    amet at? Unde voluptate sequi atque, eum nulla, ad quis
+                    eligendi assumenda error autem id, ea officia temporibus
+                    quia fuga.
+                </p>
+            </div>
+        </>
+    );
 }

1f7214960c108a0cfd557388de176a0fbf142ec7
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-08-25 22:59:00 +0330

    add category list

---
+++ products-list-task/src/components/CategoryCard.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import summarizeString from '../utils/services/summarizeString';
+
+export default function CategoryCard({categoryName}) {
+    const [categoryImageUrl,setCategoryImageUrl] = useState('');
+    useEffect(()=>{
+        const api = `https://fakestoreapi.com/products/category/${categoryName}`;
+        axios({
+            method: "get",
+            url: api,
+        })
+            .then((response) => {
+                setCategoryImageUrl(response.data[Math.floor(Math.random()*response.data.length)].image);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },[categoryName])
+  return (
+      <div className="category" key={categoryName}>
+          <div className="product-image">
+              <img src={categoryImageUrl} alt={categoryName} loading="lazy" />
+          </div>
+          <span className="title">
+              {summarizeString(categoryName, 20)}
+          </span>
+      </div>
+  );
+}
--- products-list-task/src/layouts/Categories.jsx
+++ products-list-task/src/layouts/Categories.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import axios from "axios";
+import CategoryCard from "../components/CategoryCard";
 
 export default function Categories() {
-  return (
-    <>
-    <Navbar selected={"Categories"}/>
-    </>
-  )
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        const api = "https://fakestoreapi.com/products/categories";
+        axios({
+            method: "get",
+            url: api,
+        })
+            .then((response) => {
+                setCategories(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    const renderCategories = (categories) => {
+        return categories.map((category) => {
+            return <CategoryCard categoryName={category} />;
+        });
+    };
+    return (
+        <>
+            <Navbar selected={"Categories"} />
+            <div className="categories">
+                    {renderCategories(categories)}
+            </div>
+        </>
+    );
 }
